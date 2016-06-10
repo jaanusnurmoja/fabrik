9b57f22586ee0361ba54402d9db7eff00e2ebacc
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2016-06-10 23:44:48 +0300

    Missed change - dynamical rowid (main table or joined table)

--- plugins/fabrik_element/tags/tags.php
+++ plugins/fabrik_element/tags/tags.php
@@ -48,6 +48,19 @@ class PlgFabrik_ElementTags extends PlgFabrik_ElementDatabasejoin
 	}
 
 	/**
+	 * Returns real pk that the parent_id of repeatelement refers to
+	 * Jaanus: testing purposes
+	 * 
+	 */
+	public function actualPKvalue($repeatCounter)
+	{
+		$joinPK = $this->getJoinedGroupPkVal($repeatCounter);
+		$mainPK = $this->getFormModel()->getRowId();
+		$pk = $this->getGroupModel()->isJoin() ? $joinPK : $mainPK;
+		return $pk;
+	}
+
+	/**
 	 * Returns javascript which creates an instance of the class defined in formJavascriptClass()
 	 *
 	 * @param   int  $repeatCounter  Repeat group counter
@@ -58,7 +71,7 @@ class PlgFabrik_ElementTags extends PlgFabrik_ElementDatabasejoin
 	{
 		$id = $this->getHTMLId($repeatCounter);
 		$opts = $this->getElementJSOptions($repeatCounter);
-		$opts->rowid = $this->getFormModel()->getRowId();
+		$opts->rowid = $this->actualPKvalue($repeatCounter);
 		$opts->id = $this->id;
 		$opts->listid = $this->getListModel()->getId();
 
@@ -134,7 +147,7 @@ class PlgFabrik_ElementTags extends PlgFabrik_ElementDatabasejoin
 	 */
 	protected function buildQueryWhere($data = array(), $repeatCounter = 0, $incWhere = true, $thisTableAlias = null, $opts = array(), $query = false)
 	{
-		$rowId = $this->getFormModel()->getRowId();
+		$rowId = $this->actualPKvalue($repeatCounter);
 		$db = $this->getDb();
 		$join = $this->getJoin();
 		$fk = $db->qn($join->table_join_alias . '.' . $join->table_join_key);
@@ -194,7 +207,10 @@ class PlgFabrik_ElementTags extends PlgFabrik_ElementDatabasejoin
 	protected function buildQueryJoin($query = false)
 	{
 		$db = $this->getDb();
-		$f = $db->qn($this->getJoin()->table_join_alias . '.tags');
+		
+		// Jaanus: not sure where it was needed but .tags seems obsolete as field names are built dynamically
+		
+		$f = $db->qn($this->getJoin()->table_join_alias . '.' . $join->table_key);
 
 		if ($query !== false)
 		{
